1bd3dda7f601a671f6c1cd0b9aecc6be8aea6122
Rudresh Sisode <rudranrajput@gmail.com>
2020-02-12 23:58:17 +0530

    post user input

--- src/app/post/post-create/post-component.ts
+++ src/app/post/post-create/post-component.ts
@@ -6,8 +6,16 @@ import { Component } from '@angular/core';
 })
 export class PostCreateComponent{
 
-  onAddPost(){
-    alert('Post Alert');
+  newPost="";
+
+
+  onAddPost(postInput:HTMLTextAreaElement){
+    console.dir(postInput);
+    this.newPost="NO CONTENT";
   }
 
 }
+/*
+timestamps:
+1:11:19/2:35:40
+*/
